2051b5ffd845d8464dd16062f183a5544fc9f08f
David J. Klein <kleinsound@gmail.com>
2016-05-20 22:18:46 +0000

    first stab, training converges

---
+++ net/data.py
@@ -0,0 +1,117 @@
+from __future__ import absolute_import
+from keras.utils import np_utils
+from keras.models import model_from_json
+import numpy as np
+import os
+from scipy.io import loadmat
+import pdb
+
+def load_model(defFile, weightFile):
+    # as per http://keras.io/faq/#how-can-i-save-a-keras-model
+
+    model = model_from_json(open(defFile).read())
+    model.load_weights(weightFile)
+
+    return model
+
+def load_batch(filePath, var='mfc'):
+    data = loadmat(filePath)
+    data = data[var]
+    data = np.rollaxis(data, 2)
+    data = data.reshape(data.shape[0], 1, data.shape[1], data.shape[2])
+
+    return data
+
+def load_sequence_batch(filePath, var='features'):
+    data = loadmat(filePath)
+    data = data[var]
+    data = data.swapaxes(1,2)
+
+    return data
+
+def permute_pair(X, y):
+
+    perm = np.random.permutation(X.shape[0])
+    X = X[perm, ...]
+    y = y[perm]
+
+    return X, y
+
+def split_pair(X, y, testSplit):
+
+    splitIdx = np.int(X.shape[0] * (1-testSplit))
+    (feaTrain, feaTest) = np.array_split(X, [splitIdx], axis=0)
+    (labelTrain, labelTest) = np.array_split(y, [splitIdx], axis=0)
+
+    return feaTrain, labelTrain, feaTest, labelTest
+
+def load_training(inFiles, dataVar, modelType, testSplit=0.1, negRatioTrain=10, negRatioTest=1, normalize=False, permuteBeforeSplit=(True,True)):
+
+    if modelType=='convnet':
+        data_loader = load_batch
+    elif modelType=='sequence':
+        data_loader = load_sequence_batch
+
+    # pos
+    feaTrainPos = data_loader(inFiles[0], dataVar)
+    labelTrainPos = np.ones((feaTrainPos.shape[0],), dtype='uint8')
+
+    # neg
+    feaTrainNeg = data_loader(inFiles[1], dataVar)
+    labelTrainNeg = np.zeros((feaTrainNeg.shape[0],), dtype='uint8')
+
+    # permute before split
+    if permuteBeforeSplit[0]: # pos
+        (feaTrainPos, labelTrainPos) = permute_pair(feaTrainPos, labelTrainPos)
+    if permuteBeforeSplit[1]: # neg
+        (feaTrainNeg, labelTrainNeg) = permute_pair(feaTrainNeg, labelTrainNeg)
+
+    # trim negatives to a given multiple of the number of positives
+    numNeg = np.min((np.int(feaTrainPos.shape[0] * negRatioTrain), feaTrainNeg.shape[0]))
+    feaTrainNeg = feaTrainNeg[:numNeg, ...]
+    labelTrainNeg = labelTrainNeg[:numNeg]
+
+    # split
+    # . pos
+    (feaTrainPos, labelTrainPos, feaTestPos, labelTestPos) = split_pair(feaTrainPos, labelTrainPos, testSplit)
+    # . neg
+    (feaTrainNeg, labelTrainNeg, feaTestNeg, labelTestNeg) = split_pair(feaTrainNeg, labelTrainNeg, testSplit)
+
+    # rebalance for test set
+    numNegTest = np.min((np.int(feaTestPos.shape[0] * negRatioTest), feaTestNeg.shape[0]))
+    feaTestNeg = feaTestNeg[:numNegTest, ...]
+    labelTestNeg = labelTestNeg[:numNegTest]
+
+    # join pos & neg into train & test sets
+    feaTrain = np.concatenate((feaTrainPos, feaTrainNeg), axis=0)
+    labelTrain = np.concatenate((labelTrainPos, labelTrainNeg), axis=0)
+    feaTest = np.concatenate((feaTestPos, feaTestNeg), axis=0)
+    labelTest = np.concatenate((labelTestPos, labelTestNeg), axis=0)
+
+    labelTrain = np.reshape(labelTrain, (len(labelTrain), 1))
+    labelTest = np.reshape(labelTest, (len(labelTest), 1))
+
+    # convert class vectors to binary class matrices
+    labelTrain = np_utils.to_categorical(labelTrain, 2)
+    labelTest = np_utils.to_categorical(labelTest, 2)
+
+    # normalization
+    if normalize:
+        feaTrain = np.float64(feaTrain)
+
+        feaMean = np.mean(feaTrain)
+        feaTrain -= feaMean
+        feaTrain = np.float32(feaTrain)
+        feaMax = np.max(np.abs(feaTrain))
+        feaTrain /= feaMax
+
+        feaTest = np.float64(feaTest)
+        feaTest -= feaMean
+        feaTest = np.float32(feaTest)
+        feaTest /= feaMax
+    else:
+        feaTrain = np.float32(feaTrain) / 255
+        feaTest = np.float32(feaTest) / 255
+
+    return (feaTrain, labelTrain), (feaTest, labelTest)
+
---
+++ net/train_cnn_spec.py
@@ -0,0 +1,115 @@
+'''Train a simple deep CNN on the audio mfc dataset.
+
+GPU run command:
+    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python2 train_cnn_spec.py
+
+'''
+
+from __future__ import print_function
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+import data
+from scipy.io import savemat
+import pdb
+
+
+batchSize = 8 
+numEpoch = 20
+# Best so far: rmsprop
+#optimizer = 'adadelta'
+#optimizer = 'adagrad'
+optimizer = 'rmsprop'
+#optimizer = SGD(lr=0.01, decay=0.0, momentum=0.9, nesterov=True)
+
+# the data, shuffled and split between train and test sets
+inFiles = ('spec_pos.mat',
+           'spec_neg.mat')
+
+modelDef = 'models/cnn_spec_try.json'
+modelWeights = 'cnn_spec_try.h5'
+modelInfo = 'models/cnn_spec_try.mat'
+
+(feaTrain, labelTrain), (feaTest, labelTest) = data.load_training(
+	inFiles, 'features', 'convnet', 
+	negRatioTrain=10, negRatioTest=10, 
+	permuteBeforeSplit=(False,False), testSplit=0.2, normalize=True)
+
+print('feaTrain shape:', feaTrain.shape)
+print(feaTrain.shape[0], 'train samples')
+print(feaTest.shape[0], 'test samples')
+
+# input dimensions
+inputShape = feaTrain.shape[1:]
+numClasses = labelTrain.shape[1]
+
+#pdb.set_trace()
+def build_model(inputShape, numClasses, optimizer):
+    model = Sequential()
+
+    model.add(Convolution2D(16, 3, 5, border_mode='same', input_shape=inputShape))
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(16, 3, 5))
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(2, 3)))
+    model.add(Dropout(0.25))
+
+    model.add(Convolution2D(32, 1, 3)) # best 64 or 32
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(32, 1, 3)) # best 64 or 32
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(1, 3)))
+    model.add(Dropout(0.25))
+
+    # New part
+    model.add(Convolution2D(64, 1, 3)) # best 64 or 32
+    model.add(Activation('relu'))
+
+    model.add(Convolution2D(64, 1, 3)) # best 64 or 32
+    model.add(Activation('relu'))
+
+    model.add(MaxPooling2D(pool_size=(1, 3)))
+    model.add(Dropout(0.25))
+    # end new part
+
+    model.add(Flatten())
+
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(numClasses))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+
+    return model
+
+model = build_model(inputShape, numClasses, optimizer)
+
+cbks = [ModelCheckpoint(modelWeights,
+        monitor='val_acc', save_best_only=True, mode='auto')]
+
+model.fit(feaTrain, labelTrain, batch_size=batchSize,
+          nb_epoch=numEpoch, show_accuracy=True,
+          validation_data=(feaTest, labelTest), callbacks=cbks, shuffle=True)
+
+print('Saving to '+modelInfo)
+savemat(modelInfo, {'modelDef': modelDef,'modelWeights': modelWeights})
+
+
+# Write model definition to file
+open(modelDef, 'w').write(model.to_json())
+
