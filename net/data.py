adjust fixed normalization for python derived fbank

net/data.py
@@ -110,8 +110,8 @@ def load_training(inFiles, dataVar, modelType, testSplit=0.1, negRatioTrain=10, 
         feaTest = np.float32(feaTest)
         feaTest /= feaMax
     else:
-        feaTrain = (np.float32(feaTrain)+6)/10# / 255
-        feaTest = (np.float32(feaTest)+5)/10# / 255
+        feaTrain = (np.float32(feaTrain)-7)/12# / 255
+        feaTest = (np.float32(feaTest)-7)/12# / 255
 
     return (feaTrain, labelTrain), (feaTest, labelTest)
 