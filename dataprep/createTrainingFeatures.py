2a18696d5e3283185a538f7965c588a797371bbe
David J. Klein <kleinsound@gmail.com>
2016-05-24 12:51:22 -0700

    create training features from wav files

---
+++ dataprep/createTrainingFeatures.py
@@ -0,0 +1,31 @@
+import numpy as np
+import audioproc as ap
+import sys
+from scipy.io import savemat
+import pdb
+
+def load_features(dirName, matchers):
+
+    for c, m in enumerate(matchers):
+        fea = np.stack(ap.wav2fbank_batch(dirName, m), axis=2)
+        if c == 0:
+            features = fea
+        else:
+            features = np.append(features, fea, axis=2)
+
+    return features
+
+
+# Input audio directory
+dirName = sys.argv[1]
+
+# Positive examples
+posMatchers = ['kwClip']
+features = load_features(dirName, posMatchers)
+savemat('spec_pos.mat',{'features': features})
+
+# Negative examples
+negMatchers = ['kwRevClip','speechClip','backClip']
+features = load_features(dirName, negMatchers)
+savemat('spec_neg.mat',{'features': features})
+
