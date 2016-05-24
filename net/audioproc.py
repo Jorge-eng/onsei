d282f2ede30e4357e77e47f69ec86c870eac243b
David J. Klein <kleinsound@gmail.com>
2016-05-24 10:59:56 -0700

    audio processing tools for fbank

---
+++ net/audioproc.py
@@ -0,0 +1,66 @@
+import os, glob, sndhdr
+import numpy as np
+from scipy.io import wavfile
+from features import fbank # python_speech_features
+import pdb
+
+def melfilter(N, freq):
+    melFreq = 2595*np.log10(1+freq/700)
+
+    maxF = np.max(melFreq)
+    minF = np.min(melFreq)
+    melBinWidth = (maxF-minF)/(N+1)
+    filt = np.zeros((N, len(freq)), dtype='float32')
+
+    for n in range(0, N):
+        idx = np.where( melFreq>=(n*melBinWidth+minF) & melFreq<=((n+2)*melBinWidth+minF) )
+        binWidth = len(idx)
+
+        filt[n, idx] = np.bartlett(binWidth)
+
+    return filt
+
+def wav2fbank(wavFile):
+
+    if isinstance(wavFile, str):
+        (fs, wav) = wavfile.read(wavFile)
+        assert fs == 16000 # requirement for now
+    elif isinstance(wavFile, np.ndarray):
+        wav = wavFile
+
+    winlen = 0.025
+    winstep = 0.010
+    nfft = np.int(np.power(2, np.ceil(np.log2(winlen*fs))))
+    winfunc = lambda x: np.hanning(x)
+    nfilt = 40
+
+    M, E = fbank(wav[:,0], fs, winlen=winlen, winstep=winstep, nfilt=nfilt, nfft=nfft, winfunc=winfunc)
+
+    logM = np.log(M)
+    logM = np.swapaxes(logM, 0, 1)
+
+    return logM
+
+def find_audio_files(wavDir, matcher=None):
+
+    # Find all audio files in the directory
+    allFiles = glob.glob(os.path.join(wavDir,'*.*'))
+    audioFiles = []
+    for f in allFiles:
+        if matcher is not None and matcher not in f:
+            continue
+        chk = sndhdr.what(f)
+        if chk is not None:
+            audioFiles.append(f)
+
+    return audioFiles
+
+def wav2fbank_batch(wavDir, matcher=None):
+
+    audioFiles = find_audio_files(wavDir, matcher)
+    logM = []
+    for f in audioFiles:
+        logM.append(wav2fbank(f))
+
+    return logM
+
--- net/cnn_predict_spec.py
+++ net/cnn_predict_spec.py
@@ -1,15 +1,8 @@
 from keras.models import model_from_json
-from features import fbank # python_speech_features
-from scipy.io import loadmat, savemat, wavfile
+from scipy.io import loadmat, savemat
+import data
 import numpy as np
 import pdb
-
-def load_model(defFile, weightFile):
-
-    model = model_from_json(open(defFile).read())
-    model.load_weights(weightFile)
-
-    return model
 
 def predict(x, model):
 
@@ -19,25 +12,9 @@ def predict(x, model):
 
     return prob
 
-def melfilter(N, freq):
-    melFreq = 2595*np.log10(1+freq/700)
-
-    maxF = np.max(melFreq)
-    minF = np.min(melFreq)
-    melBinWidth = (maxF-minF)/(N+1)
-    filt = np.zeros((N, len(freq)), dtype='float32')
-
-    for n in range(0, N):
-        idx = np.where( melFreq>=(n*melBinWidth+minF) & melFreq<=((n+2)*melBinWidth+minF) )
-        binWidth = len(idx)
-
-        filt[n, idx] = np.bartlett(binWidth)
-
-    return filt
-
 def fbank_stack(wavFile, winLen, winShift):
 
-    logM = wav2fbank(wavFile)
+    logM = data.wav2fbank(wavFile)
     nBands = logM.shape[0]
     nFrames = logM.shape[1]
 
@@ -50,31 +27,8 @@ def fbank_stack(wavFile, winLen, winShift):
 
     return stack
 
-def wav2fbank(wavFile):
-
-    if isinstance(wavFile, str):
-        (fs, wav) = wavfile.read(wavFile)
-        assert fs == 16000 # requirement for now
-    elif isinstance(wavFile, np.ndarray):
-        wav = wavFile
-
-    winlen = 0.025
-    winstep = 0.010
-    nfft = np.int(np.power(2, np.ceil(np.log2(winlen*fs))))
-    winfunc = lambda x: np.hanning(x)
-    nfilt = 40
-
-    M, E = fbank(wav[:,0], fs, winlen=winlen, winstep=winstep, nfilt=nfilt, nfft=nfft, winfunc=winfunc)
-
-    logM = np.log(M)
-    logM = np.swapaxes(logM, 0, 1)
-
-    return logM
-
 #wavFile = '160517_04-2_16k.WAV'
 #stack = fbank_stack(wavFile, 199, 20)
 #savemat('stackpy.mat',{'stack':stack})
 
-#logM = wav2fbank(wavFile)
-#savemat('logMpy.mat',{'logM':logM})
 
