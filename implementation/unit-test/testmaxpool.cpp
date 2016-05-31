fdc0dc5496803e730859048290a480db7a2ec9b8
Benjamin Joseph <benjo@sayhello.com>
2016-05-31 13:33:33 -0700

    maxpool can handle it if there is some leftover

--- implementation/unit-test/testmaxpool.cpp
+++ implementation/unit-test/testmaxpool.cpp
@@ -70,6 +70,119 @@ TEST_F(TestMaxPool, OneImageSquare) {
     
 }
 
+TEST_F(TestMaxPool, OneImageWithLeftoverCols) {
+    //one layer
+    const uint32_t input_dims[] = {1,1,4,4};
+    tensor_in = tinytensor_create_new_tensor(input_dims);
+    
+    const uint32_t pool_dims[] = {2,3};
+    const uint32_t output_dims[] = {1,1,2,2};
+    
+    //  0  1  2  3
+    //  4  5  6  7
+    //  8  9  10 11
+    //  12 13 14 15
+    
+    //  6  7
+    //  14 15
+    
+    const Weight_t ref[] = {6,7,14,15};
+    
+    for (int i = 0; i < 16; i++) {
+        tensor_in->x[i] = i;
+    }
+    
+    const MaxPoolLayer_t max_pool_layer_def = {pool_dims,output_dims,input_dims};
+    
+    ConstLayer_t layer = tinytensor_create_maxpool_layer(&max_pool_layer_def);
+    
+    tensor_out = tinytensor_create_new_tensor(output_dims);
+    
+    layer.eval(layer.context,tensor_out,tensor_in);
+    
+    
+    for (int j = 0; j < 4; j++) {
+        ASSERT_EQ(ref[j], tensor_out->x[j]);
+    }
+    
+    
+}
+
+TEST_F(TestMaxPool, OneImageWithLeftoverRows) {
+    //one layer
+    const uint32_t input_dims[] = {1,1,4,4};
+    tensor_in = tinytensor_create_new_tensor(input_dims);
+    
+    const uint32_t pool_dims[] = {3,2};
+    const uint32_t output_dims[] = {1,1,2,2};
+    
+    //  0  1  2  3
+    //  4  5  6  7
+    //  8  9  10 11
+    //  12 13 14 15
+    
+    //  6  7
+    //  14 15
+    
+    const Weight_t ref[] = {9,11,13,15};
+    
+    for (int i = 0; i < 16; i++) {
+        tensor_in->x[i] = i;
+    }
+    
+    const MaxPoolLayer_t max_pool_layer_def = {pool_dims,output_dims,input_dims};
+    
+    ConstLayer_t layer = tinytensor_create_maxpool_layer(&max_pool_layer_def);
+    
+    tensor_out = tinytensor_create_new_tensor(output_dims);
+    
+    layer.eval(layer.context,tensor_out,tensor_in);
+    
+    
+    for (int j = 0; j < 4; j++) {
+        ASSERT_EQ(ref[j], tensor_out->x[j]);
+    }
+    
+    
+}
+
+TEST_F(TestMaxPool, OneImageWithLeftoverRowsAndCols) {
+    //one layer
+    const uint32_t input_dims[] = {1,1,4,4};
+    tensor_in = tinytensor_create_new_tensor(input_dims);
+    
+    const uint32_t pool_dims[] = {3,3};
+    const uint32_t output_dims[] = {1,1,2,2};
+    
+    //  0  1  2  3
+    //  4  5  6  7
+    //  8  9  10 11
+    //  12 13 14 15
+    
+    //  6  7
+    //  14 15
+    
+    const Weight_t ref[] = {10,11,14,15};
+    
+    for (int i = 0; i < 16; i++) {
+        tensor_in->x[i] = i;
+    }
+    
+    const MaxPoolLayer_t max_pool_layer_def = {pool_dims,output_dims,input_dims};
+    
+    ConstLayer_t layer = tinytensor_create_maxpool_layer(&max_pool_layer_def);
+    
+    tensor_out = tinytensor_create_new_tensor(output_dims);
+    
+    layer.eval(layer.context,tensor_out,tensor_in);
+    
+    
+    for (int j = 0; j < 4; j++) {
+        ASSERT_EQ(ref[j], tensor_out->x[j]);
+    }
+    
+    
+}
 
 TEST_F(TestMaxPool, TwoImageSquare) {
     //one layer
