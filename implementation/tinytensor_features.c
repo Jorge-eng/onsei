000d621a72d034f7a4b412dbfaaaa9c56c3a04c1
Benjamin Joseph <benjo@sayhello.com>
2016-06-01 17:03:12 -0700

    stop point for tonight -- nothing works, aaaagh

--- implementation/tinytensor_features.c
+++ implementation/tinytensor_features.c
@@ -1,6 +1,5 @@
 #include "tinytensor_features.h"
 #include "tinytensor_memory.h"
-#include "tinytensor_math.h"
 
 #include "hellomath/fft.h"
 #include "hellomath/hellomath.h"
@@ -11,6 +10,11 @@
 #define FFT_UNPADDED_SIZE (400) 
 #define NUM_MEL_BINS (40)
 #define PREEMPHASIS (TOFIX(0.95,15))
+
+#define QFIXEDPOINT_INT16 (15)
+
+#define MUL16(a,b)\
+((int16_t)(((int32_t)(a * b)) >> QFIXEDPOINT_INT16))
 
 const static int16_t k_hanning[FFT_UNPADDED_SIZE] = {0,2,8,18,32,51,73,99,130,164,203,245,292,342,397,455,517,584,654,728,806,888,973,1063,1156,1253,1354,1459,1567,1679,1794,1914,2036,2163,2293,2426,2563,2703,2847,2994,3144,3298,3455,3615,3778,3944,4114,4286,4462,4640,4821,5006,5193,5383,5575,5770,5968,6169,6372,6577,6785,6995,7208,7423,7640,7859,8080,8304,8529,8757,8986,9217,9450,9684,9921,10159,10398,10639,10881,11125,11370,11616,11863,12112,12362,12612,12864,13116,13369,13623,13878,14133,14389,14645,14902,15159,15417,15674,15932,16190,16448,16706,16964,17222,17479,17736,17993,18250,18506,18762,19016,19271,19524,19777,20029,20280,20530,20779,21027,21274,21520,21764,22007,22249,22489,22728,22965,23200,23434,23666,23896,24124,24351,24575,24798,25018,25236,25452,25666,25877,26086,26293,26497,26699,26898,27095,27289,27480,27668,27854,28037,28216,28393,28567,28738,28906,29071,29233,29391,29546,29698,29847,29992,30134,30273,30408,30540,30668,30792,30913,31031,31145,31255,31361,31464,31563,31658,31749,31837,31921,32001,32077,32149,32217,32281,32342,32398,32451,32499,32544,32584,32620,32653,32681,32706,32726,32742,32754,32762,32766,32766,32762,32754,32742,32726,32706,32681,32653,32620,32584,32544,32499,32451,32398,32342,32281,32217,32149,32077,32001,31921,31837,31749,31658,31563,31464,31361,31255,31145,31031,30913,30792,30668,30540,30408,30273,30134,29992,29847,29698,29546,29391,29233,29071,28906,28738,28567,28393,28216,28037,27854,27668,27480,27289,27095,26898,26699,26497,26293,26086,25877,25666,25452,25236,25018,24798,24575,24351,24124,23896,23666,23434,23200,22965,22728,22489,22249,22007,21764,21520,21274,21027,20779,20530,20280,20029,19777,19524,19271,19016,18762,18506,18250,17993,17736,17479,17222,16964,16706,16448,16190,15932,15674,15417,15159,14902,14645,14389,14133,13878,13623,13369,13116,12864,12612,12362,12112,11863,11616,11370,11125,10881,10639,10398,10159,9921,9684,9450,9217,8986,8757,8529,8304,8080,7859,7640,7423,7208,6995,6785,6577,6372,6169,5968,5770,5575,5383,5193,5006,4821,4640,4462,4286,4114,3944,3778,3615,3455,3298,3144,2994,2847,2703,2563,2426,2293,2163,2036,1914,1794,1679,1567,1459,1354,1253,1156,1063,973,888,806,728,654,584,517,455,397,342,292,245,203,164,130,99,73,51,32,18,8,2,0};
 
@@ -45,18 +49,18 @@ static void add_to_buffer(const int16_t * samples, const uint32_t num_samples) {
     const int32_t memleft = _this.end - _this.pbuf;
     int32_t chunk_size = memleft > num_samples ? num_samples : memleft;
 
+    //printf("offset=%d, chunk_size=%d, total=%d\n",_this.pbuf - _this.buf,chunk_size,chunk_size + (int32_t)(_this.pbuf - _this.buf));
     MEMCPY(_this.pbuf,samples,chunk_size*sizeof(int16_t));
-        
+    _this.pbuf += chunk_size;
+
     if (chunk_size < num_samples) {
         const int32_t chunk_size2 = num_samples - chunk_size;
         _this.pbuf = _this.buf;
+    //    printf("samples offset = %d\n",&samples[chunk_size] - samples);
         MEMCPY(_this.pbuf,&samples[chunk_size],chunk_size2 * sizeof(int16_t));
-        _this.pbuf += chunk_size;
+        _this.pbuf += chunk_size2;
     }
-    else {
-        _this.pbuf += num_samples;
-    }
-    
+   
     _this.num_samples_left_to_process += num_samples;
 }
 
@@ -106,9 +110,7 @@ static uint8_t add_samples_and_get_mel(int16_t * melbank, const int16_t * sample
     
     read_out_buffer(fr,FFT_UNPADDED_SIZE);
     
-    return 0;
-
-    
+  
     //"preemphasis"
     for (i = 1; i < FFT_UNPADDED_SIZE; i++) {
         temp16 = MUL16(fr[i-1], preemphasis_coeff);
@@ -143,30 +145,24 @@ static uint8_t add_samples_and_get_mel(int16_t * melbank, const int16_t * sample
 
 void tinytensor_features_add_samples(const int16_t * samples, const uint32_t num_samples) {
     int16_t melbank[NUM_MEL_BINS];
-    int16_t scaled_melbank[NUM_MEL_BINS];
     uint32_t i;
     if (add_samples_and_get_mel(melbank,samples,num_samples)) {
         
-        for (i = 0; i < NUM_MEL_BINS; i++) {
-            melbank[i] >>= 5;
-            if (melbank[i] < -MAX_WEIGHT) {
-                melbank[i] = -MAX_WEIGHT;
+        for (i = 0; i < 40; i++) {
+            if (i!= 0) {
+                printf (",");
             }
-           
-            if (melbank[i] > MAX_WEIGHT) {
-                melbank[i] = MAX_WEIGHT;
-            }
-        
-            scaled_melbank[i] = melbank[i];
-            
+            printf("%d",melbank[i]);
         }
         
+        printf("\n");
+        
         for (i = 5; i < NUM_MEL_BINS; i++) {
-            if (scaled_melbank[i] > 0) {
+            if (melbank[i] > 0) {
                 int foo = 3;
                 foo++;
             }
-
+            
         }
         
         //TODO add to circular buffer for mel bins
