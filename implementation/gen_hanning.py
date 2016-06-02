e8d4d1e05a54ca3cd58c24f9f37bdaf1980e5ba1
Benjamin Joseph <benjo@sayhello.com>
2016-06-02 15:18:29 -0700

    found overflow, now it does not on chirp

--- implementation/gen_hanning.py
+++ implementation/gen_hanning.py
@@ -2,7 +2,7 @@
 import numpy as np
 N = 400
 
-h = np.hanning(N)
+h = np.hanning(N) * 0.84
 x = np.round(h * 32767)
 
 print ','.join(x.astype(int).astype(str).tolist())
--- implementation/tinytensor.cpp
+++ implementation/tinytensor.cpp
@@ -24,7 +24,6 @@ int main(int argc, char * argv[]) {
     printf ("    Channels    : %d\n", file.channels ()) ;
     printf ("    Frames      : %d\n", file.frames ()) ;
     */
-    uint32_t n = file.frames();
     std::vector<int16_t> mono_samples;
     mono_samples.reserve(file.frames());
     int16_t buf[BUF_SIZE];
@@ -48,7 +47,7 @@ int main(int argc, char * argv[]) {
             int16_t tempbuf[NUM_SAMPLES_TO_RUN_FFT];
             memset(tempbuf,0xFF,sizeof(tempbuf));
             for (int t= 0; t < NUM_SAMPLES_TO_RUN_FFT; t++) {
-                tempbuf[t] = mono_samples[i + t];
+                tempbuf[t] = (int) (1.0 * mono_samples[i + t]);
             }
             
             tinytensor_features_add_samples(tempbuf, NUM_SAMPLES_TO_RUN_FFT);
