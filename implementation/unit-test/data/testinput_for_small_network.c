fe5d9f034812b5a43dec5f30005e4a266a7f1d8f
Benjamin Joseph <benjo@sayhello.com>
2016-06-05 22:19:28 -0700

    problem in python generator code... somwhere.

---
+++ implementation/unit-test/data/testinput_for_small_network.c
@@ -0,0 +1,4 @@
+const static Weight_t testinput_smallnet_x[12] = {120,48,124,70,5,73,58,75,100,34,96,70};
+const static uint32_t testinput_smallnet_dims[4] = {1,2,2,3};
+const static ConstTensor_t testinput_smallnet = {&testinput_smallnet_x[0],&testinput_smallnet_dims[0]};
+
