5351f2f827ee2e803f85b657d1983c1b16ef8d19
David J. Klein <kleinsound@gmail.com>
2016-06-05 21:23:30 -0700

    Use tinyfeats in main directory

--- implementation/batchfeat.sh
+++ implementation/batchfeat.sh
@@ -1,1 +1,1 @@
-find $1 -type f -iname \*.wav | xargs -I {} basename {} | xargs -I {} ./build_commandline/tinyfeats $1/{} $2/{}.bin
+find $1 -type f -iname \*.wav | xargs -I {} basename {} | xargs -I {} ./tinyfeats $1/{} $2/{}.bin
