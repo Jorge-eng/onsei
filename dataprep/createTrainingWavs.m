155ab008c790ac77df0d0a07ae96bcee8283c91e
David J. Klein <david@ersatzlabs.com>
2016-06-08 11:57:22 -0700

    createQuickyTrainingWavs -> createTrainingWavs

---
+++ dataprep/createTrainingWavs.m
@@ -0,0 +1,26 @@
+function createQuickyTrainingWavs(dataFile, outDir)
+% createQuickyTrainingWavs(dataFile, outDir)
+
+load(dataFile, 'info')
+[~,~] = mkdir(outDir);
+
+Fs = 16000;
+
+vars = {'kwClip','kwRevClip','speechClip','backClip',...
+        'earlyImplantClip','lateImplantClip',...
+        'partialEarlyClip','partialLateClip',...
+        'shiftEarlyClip','shiftLateClip'};
+    
+for v = 1:length(vars)
+    var = vars{v};
+    disp(['Creating ' var])
+    data = load(dataFile, var);
+    nFiles = length(data.(var));
+    for n = 1:nFiles
+        [~,fileTag] = fileparts(info.fileNames{n});
+        for s = 1:length(data.(var){n})
+            wav = resample(data.(var){n}{s},16,48);
+            audiowrite(fullfile(outDir,[var '_' fileTag '_' num2str(s) '.wav']), wav, Fs)
+        end
+    end
+end
