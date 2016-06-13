0b6cc8eaf653c5aa2d51df5eeca3a5341fa1e292
David J. Klein <david@ersatzlabs.com>
2016-06-13 05:25:57 -0700

    support embedded features

--- dataprep/createTestGrid.py
+++ dataprep/createTestGrid.py
@@ -1,9 +1,10 @@
 import numpy as np
-import audioproc as ap
-from createTrainingFeatures import load_features, get_conditions
-import sys
+from createTrainingFeatures import load_features as load_features_python
+from loadEmbeddedFeatures import load_features as load_features_tinyfeats
+from createTrainingFeatures import get_conditions
+import sys, os
+import glob
 from scipy.io import savemat
-import pickle
 import pdb
 
 # Input audio directory
@@ -15,12 +16,20 @@ condMatch = pos + neg
 
 idMatch = ['160517_08','160606_03','160606_04']
 
+if len(glob.glob(os.path.join(dirName,'*.wav'))) > 0:
+    load_features = load_features_python
+    normalizer = lambda x: (x - 7) / 12
+elif len(glob.glob(os.path.join(dirName,'*.bin'))) > 0:
+    load_features = load_features_tinyfeats
+    normalizer = lambda x: ((np.float32(x) + 80)/ 140 * 12) + 7
+
 identity = []
 sampleType = []
 features = np.array([])
 for i in idMatch:
     for c in condMatch:
         fea = load_features(dirName, [c+'_'+i])
+
         identity.extend([i]*fea.shape[2])
         sampleType.extend([c]*fea.shape[2])
         if features.shape[0] == 0:
@@ -29,7 +38,7 @@ for i in idMatch:
             features = np.append(features, fea, axis=2)
 
 # Normalization (Todo...)
-features = (features - 7) / 12
+features = normalizer(features)
 
 savemat(outName, {'features': features, 'identity': identity, 'sampleType': sampleType,
                   'idMatch': idMatch, 'condMatch': condMatch})
