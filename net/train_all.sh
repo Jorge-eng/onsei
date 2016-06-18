9fdd1092fd82b84ddba98f1175635ee4fa61fae9
David J. Klein <kleinsound@gmail.com>
2016-06-18 20:34:29 +0000

    no prompt on unzip

--- net/train_all.sh
+++ net/train_all.sh
@@ -1,25 +1,25 @@
 
 model=model_jun17_small_sigm
 
-unzip trainingFeats.zip 
+unzip -o trainingFeats.zip 
 python train_spec.py $model cnn
 cd models
 ./zip_model.sh model_jun17_small_sigm _clean
 cd -
 
-unzip trainingFeatsNoise.zip 
+unzip -o trainingFeatsNoise.zip 
 python train_spec.py $model cnn
 cd models
 ./zip_model.sh model_jun17_small_sigm _noise
 cd -
 
-unzip trainingTinyfeats.zip 
+unzip -o trainingTinyfeats.zip 
 python train_spec.py $model cnn
 cd models
 ./zip_model.sh model_jun17_small_sigm _clean_tiny
 cd -
 
-unzip trainingTinyfeatsNoise.zip 
+unzip -o trainingTinyfeatsNoise.zip 
 python train_spec.py $model cnn
 cd models
 ./zip_model.sh model_jun17_small_sigm _noise_tiny
