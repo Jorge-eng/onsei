e236837ef60baf346e62ca8ab8eefb2025da7960
David J. Klein <david@ersatzlabs.com>
2016-06-18 12:26:21 -0700

    helper script

---
+++ dataprep/wav2feats.sh
@@ -0,0 +1,19 @@
+# Example: 
+# $ ./wav2feats.sh train ~/Dropbox/Data/keyword/trainingWavs trainingFeats
+# $ ./wav2feats.sh test ~/Dropbox/Data/keyword/testingWavs testingFeats.mat
+
+if [ $1 = "train" ]; then
+    echo "createTrainingFeatures.py"
+    python createTrainingFeatures.py $2
+
+    echo "zip"
+    rm -f $3\.zip 
+    zip -r $3\.zip spec_pos.mat spec_neg.mat
+
+    echo "copyToHelloServer.sh"
+    ~/copyToHelloServer.sh $3\.zip onsei/net
+elif [ $1 = "test" ]; then
+    echo "createTestGrid.py"
+    python createTestGrid.py $2 $3 
+fi
+
