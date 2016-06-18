873165ac292c4e59c617ff7956edbadd08221228
David J. Klein <kleinsound@gmail.com>
2016-06-18 20:41:44 +0000

    arrange into oneliners

--- net/train_all.sh
+++ net/train_all.sh
@@ -1,27 +1,15 @@
 
 model=model_jun17_small_sigm
 
-unzip -o trainingFeats.zip 
-python train_spec.py $model cnn
-cd models
-./zip_model.sh model_jun17_small_sigm _clean
-cd -
+unzip -o trainingFeats.zip ; python train_spec.py $model cnn
+cd models ; ./zip_model.sh model_jun17_small_sigm _clean ; cd -
 
-unzip -o trainingFeatsNoise.zip 
-python train_spec.py $model cnn
-cd models
-./zip_model.sh model_jun17_small_sigm _noise
-cd -
+unzip -o trainingFeatsNoise.zip ; python train_spec.py $model cnn
+cd models ; ./zip_model.sh model_jun17_small_sigm _noise ; cd -
 
-unzip -o trainingTinyfeats.zip 
-python train_spec.py $model cnn
-cd models
-./zip_model.sh model_jun17_small_sigm _clean_tiny
-cd -
+unzip -o trainingTinyfeats.zip ; python train_spec.py $model cnn
+cd models ; ./zip_model.sh model_jun17_small_sigm _clean_tiny ; cd -
 
-unzip -o trainingTinyfeatsNoise.zip 
-python train_spec.py $model cnn
-cd models
-./zip_model.sh model_jun17_small_sigm _noise_tiny
-cd -
+unzip -o trainingTinyfeatsNoise.zip ; python train_spec.py $model cnn
+cd models ; ./zip_model.sh model_jun17_small_sigm _noise_tiny ; cd -
 
