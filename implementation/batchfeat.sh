f332f1c826faac054b62c6ae1696f7caed83977e
David J. Klein <david@ersatzlabs.com>
2016-06-17 20:58:24 -0700

    allow to callable from dataprep

--- implementation/batchfeat.sh
+++ implementation/batchfeat.sh
@@ -1,2 +1,2 @@
 mkdir -p $2
-find $1 -type f -iname \*.wav | xargs -I {} basename {} | xargs -I {} ./tinyfeats $1/{} $2/{}.bin
+find $1 -type f -iname \*.wav | xargs -I {} basename {} | xargs -I {} ../implementation/tinyfeats $1/{} $2/{}.bin
