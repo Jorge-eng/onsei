3d251ec740ce1b818e344bfe45705a44b4310971
David J. Klein <kleinsound@gmail.com>
2016-06-30 23:47:33 +0000

    added model_jun30_cnn_lstm

--- net/modeldefs.py
+++ net/modeldefs.py
@@ -1,8 +1,11 @@
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.core import Reshape, Permute
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.layers.recurrent import LSTM
 from keras.optimizers import SGD
+import numpy as np
+import pdb
 
 def model_may25_lstm_small(inputShape, numClasses):
 
@@ -250,6 +253,29 @@ def model_jun22_smaller_sigm(inputShape, numClasses):
 
     model.add(Dense(numClasses))
     model.add(Activation('sigmoid'))
+
+    return model, optimizer, loss
+
+def model_jun30_cnn_lstm(inputShape, numClasses):
+
+    optimizer = 'rmsprop'
+    loss = 'categorical_crossentropy'
+
+    model = Sequential()
+
+    model.add(Convolution2D(4, 3, 5, border_mode='valid', input_shape=inputShape))
+    model.add(MaxPooling2D(pool_size=(2, 3)))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.25))
+    convOutShape = model.layers[-1].output_shape
+    model.add(Reshape((np.prod(convOutShape[1:3]), convOutShape[3])))
+    model.add(Permute((2, 1)))
+    #model.add(LSTM(64, return_sequences=True))
+    #model.add(Dropout(0.2))
+    model.add(LSTM(32, return_sequences=False))
+    model.add(Dropout(0.2))
+    model.add(Dense(numClasses))
+    model.add(Activation('softmax'))
 
     return model, optimizer, loss
 
