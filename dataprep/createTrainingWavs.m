29a3d4e3f2d7a0f13437698fd75cb544cda13a6a
David J. Klein <david@ersatzlabs.com>
2016-07-05 22:33:36 -0700

    memory scalable data reorg and 16k sampling

--- dataprep/createTrainingWavs.m
+++ dataprep/createTrainingWavs.m
@@ -1,26 +1,36 @@
 function createTrainingWavs(dataFile, outDir)
 % createTrainingWavs(dataFile, outDir)
 
-load(dataFile, 'info')
 [~,~] = mkdir(outDir);
 
 Fs = 16000;
+
+d = whos('-file',dataFile,'file_*');
+fileVars = {d.name};
 
 vars = {'kwClip','kwRevClip','speechClip','backClip',...
         'earlyImplantClip','lateImplantClip',...
         'partialEarlyClip','partialLateClip',...
         'shiftEarlyClip','shiftLateClip'};
+
+load(dataFile, 'info')
+
+for f = 1:length(fileVars)
+    disp(['Loading ' fileVars{f}])
+    fileTag = strrep(fileVars{f},'file_','');
     
-for v = 1:length(vars)
-    var = vars{v};
-    disp(['Creating ' var])
-    data = load(dataFile, var);
-    nFiles = length(data.(var));
-    for n = 1:nFiles
-        [~,fileTag] = fileparts(info.fileNames{n});
-        for s = 1:length(data.(var){n})
-            wav = resample(data.(var){n}{s},16,48);
+    d = load(dataFile, fileVars{f});
+    data = d.(fileVars{f});
+    
+    for v = 1:length(vars)
+        var = vars{v};
+        disp(['Creating ' var])
+        
+        for s = 1:length(data.(var))
+            %wav = resample(data.(var){n}{s},16,48);
+            wav = data.(var){s};
             audiowrite(fullfile(outDir,[var '_' fileTag '_' num2str(s) '.wav']), wav, Fs)
         end
+        
     end
 end
