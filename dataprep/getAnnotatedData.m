a82c057ed0fa27ac78b94146a2cd4bee1315b3b0
David J. Klein <david@ersatzlabs.com>
2016-07-08 11:14:51 -0700

    use readAnnotations.m

--- dataprep/getAnnotatedData.m
+++ dataprep/getAnnotatedData.m
@@ -5,34 +5,7 @@ if ~exist('dataFile','var')
     dataFile = 'clipData.mat';
 end
 
-fileName = {};
-clipStart = [];
-clipEnd = [];
-label = {};
-midPt = [];
-
-fid = fopen(csvFile,'r');
-while 1
-    line = fgetl(fid);
-    if ~ischar(line)
-        break
-    end
-    
-    line = strsplit(line,',');
-    
-    fileName{end+1} = line{1};
-    clipStart(end+1) = str2double(line{2});
-    clipEnd(end+1) = str2double(line{3});
-    label{end+1} = line{4};
-    if strcmp('keyword',label{end})
-        midPt(end+1) = str2double(line{5});
-    else
-        midPt(end+1) = NaN;
-    end
-end
-fclose(fid);
-
-fileNames = unique(fileName);
+[fileNames, fileName, clipStart, clipEnd, label, midPt] = readAnnotations(csvFile);
 
 info.fileNames = fileNames;
 info.fileName = fileName;
---
+++ dataprep/readAnnotations.m
@@ -0,0 +1,31 @@
+function [fileNames, fileName, clipStart, clipEnd, label, midPt] = readAnnotations(csvFile)
+%[fileNames, fileName, clipStart, clipEnd, label, midPt] = readAnnotations(csvFile)
+
+fileName = {};
+clipStart = [];
+clipEnd = [];
+label = {};
+midPt = [];
+
+fid = fopen(csvFile,'r');
+while 1
+    line = fgetl(fid);
+    if ~ischar(line)
+        break
+    end
+    
+    line = strsplit(line,',');
+    
+    fileName{end+1} = line{1};
+    clipStart(end+1) = str2double(line{2});
+    clipEnd(end+1) = str2double(line{3});
+    label{end+1} = line{4};
+    if strcmp('keyword',label{end})
+        midPt(end+1) = str2double(line{5});
+    else
+        midPt(end+1) = NaN;
+    end
+end
+fclose(fid);
+
+fileNames = unique(fileName);
