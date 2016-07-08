1dbfe2e21d8cb5e1cb35e0d5af3cd6fa98e2366b
David J. Klein <david@ersatzlabs.com>
2016-07-08 01:52:02 -0700

    apply random impulse responses

--- dataprep/getAnnotatedData.m
+++ dataprep/getAnnotatedData.m
@@ -52,6 +52,10 @@ noiseFiles = {d.name};
 noiseScales = 12:3:30;
 %noiseScales = Inf;
 
+irDir = 'IR';
+d = dir(fullfile(irDir,'*16k.wav'));
+irFiles = {d.name};
+
 for j = 1:length(fileNames)
     disp(['File ' num2str(j) ' out of ' num2str(length(fileNames))])
     fileLoc = find(strcmp(fileName, fileNames{j}));
@@ -89,6 +93,8 @@ for j = 1:length(fileNames)
         idx = fileLoc(ii(k));
         speech = [speech; audioGet(fn, [clipStart(idx) clipEnd(idx)])];
     end
+   
+    %tau = getReverbTime(fn);
     
     ii = find(strcmp('keyword', label(fileLoc)));
     for k = 1:length(ii)
@@ -128,7 +134,8 @@ for j = 1:length(fileNames)
         
         kwVoc = applyRandomShift(kw, Fs);
         noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-        data.(fileField).kwClip{end+1} = kwVoc + noise;
+        mix = applyRandomIR(kwVoc + noise, irDir, irFiles);
+        data.(fileField).kwClip{end+1} = mix;
         
         % reversed keyword
         x = kwVoc;
@@ -136,7 +143,8 @@ for j = 1:length(fileNames)
         in = flipud(x(pad:pad+cd,:));
         x(pad:pad+cd,:) = (1-window).*out + window.*in;
         noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-        data.(fileField).kwRevClip{end+1} = x + noise;
+        mix = applyRandomIR(x + noise, irDir, irFiles);
+        data.(fileField).kwRevClip{end+1} = mix;
         
         % speech implants
         starts = 1 + floor(rand(1, alignedSpeechPerKw)*(length(speech)-clipLen));
@@ -147,14 +155,16 @@ for j = 1:length(fileNames)
             x(pad:pad+cd,:) = (1-window).*out + window.*in;
             x = applyRandomShift(x, Fs);
             noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-            data.(fileField).speechClip{end+1} = x + noise;
+            mix = applyRandomIR(x + noise, irDir, irFiles);
+            data.(fileField).speechClip{end+1} = mix;
         end
         starts = 1 + floor(rand(1, randomSpeechPerKw)*(length(speech)-clipLen));
         for n = 1:randomSpeechPerKw
             x = gain*speech(starts(n)+1:starts(n)+clipLen,:);
             x = applyRandomShift(x, Fs);
             noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-            data.(fileField).speechClip{end+1} = x + noise;
+            mix = applyRandomIR(x + noise, irDir, irFiles);
+            data.(fileField).speechClip{end+1} = mix;
         end
         
         % partials --- 
@@ -171,7 +181,8 @@ for j = 1:length(fileNames)
         x(impInd,:) = (1-window).*out + window.*in;
         x = applyRandomShift(x, Fs);
         noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-        data.(fileField).earlyImplantClip{end+1} = x + noise;
+        mix = applyRandomIR(x + noise, irDir, irFiles);
+        data.(fileField).earlyImplantClip{end+1} = mix;
         
         % partial late - missing early part
         x = kw;
@@ -180,7 +191,8 @@ for j = 1:length(fileNames)
         x(impInd,:) = (1-window).*out + window.*in;
         x = applyRandomShift(x, Fs);
         noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-        data.(fileField).partialLateClip{end+1} = x + noise;
+        mix = applyRandomIR(x + noise, irDir, irFiles);
+        data.(fileField).partialLateClip{end+1} = mix;
         
         % partial implant late
         x = kw;
@@ -192,7 +204,8 @@ for j = 1:length(fileNames)
         x(impInd,:) = (1-window).*out + window.*in;
         x = applyRandomShift(x, Fs);
         noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-        data.(fileField).lateImplantClip{end+1} = x + noise;
+        mix = applyRandomIR(x + noise, irDir, irFiles);
+        data.(fileField).lateImplantClip{end+1} = mix;
         
         % partial early -  missing late part
         x = kw;
@@ -201,12 +214,13 @@ for j = 1:length(fileNames)
         x(impInd,:) = (1-window).*out + window.*in;
         x = applyRandomShift(x, Fs);
         noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-        data.(fileField).partialEarlyClip{end+1} = x + noise;
+        mix = applyRandomIR(x + noise, irDir, irFiles);
+        data.(fileField).partialEarlyClip{end+1} = mix;
             
         % Partials shifted to boundaries --
         % The end
         if padIn + (cdIn-kwMidIn) < csIn
-            kwSh = audioGet(fn, [csIn-(padIn+cdIn-kwMidIn)+1 csIn-(padIn+cdIn-kwMidIn)+clipLenIn]);
+            kwSh = gain*audioGet(fn, [csIn-(padIn+cdIn-kwMidIn)+1 csIn-(padIn+cdIn-kwMidIn)+clipLenIn]);
             impInd = 1:cd-kwMid;
             window = getWindow(length(impInd), round(0.05*Fs), size(kw,2));
             out = kwSh(impInd,:);
@@ -215,11 +229,12 @@ for j = 1:length(fileNames)
             kwSh(impInd,:) = (1-window).*out + window.*in;
             kwSh = applyRandomShift(kwSh, Fs);
             noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-            data.(fileField).shiftLateClip{end+1} = kwSh + noise;
+            mix = applyRandomIR(kwSh + noise, irDir, irFiles);
+            data.(fileField).shiftLateClip{end+1} = mix;
         end
         % The beginning
         if csIn+kwMidIn+clipLenIn <= auInfo.TotalSamples
-            kwSh = audioGet(fn, [csIn+kwMidIn+1 csIn+kwMidIn+clipLenIn]);
+            kwSh = gain*audioGet(fn, [csIn+kwMidIn+1 csIn+kwMidIn+clipLenIn]);
             impInd = clipLen-kwMid+1:clipLen;
             window = getWindow(length(impInd), round(0.05*Fs), size(kw,2));
             out = kwSh(impInd,:);
@@ -228,7 +243,8 @@ for j = 1:length(fileNames)
             kwSh(impInd,:) = (1-window).*out + window.*in;
             kwSh = applyRandomShift(kwSh, Fs);
             noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
-            data.(fileField).shiftEarlyClip{end+1} = kwSh + noise;
+            mix = applyRandomIR(kwSh + noise, irDir, irFiles);
+            data.(fileField).shiftEarlyClip{end+1} = mix;
         end
             
     end
@@ -240,7 +256,8 @@ for j = 1:length(fileNames)
         noise = getRandomNoise(noiseDir, noiseFiles, noiseScales, clipLen, nCh);
         x = gain*background(starts(n):(starts(n)-1)+clipLen,:);
         x = applyRandomShift(x, Fs);
-        data.(fileField).backClip{end+1} = x + noise;
+        mix = applyRandomIR(x + noise, irDir, irFiles);
+        data.(fileField).backClip{end+1} = mix;
     end
     
     disp(['Saving ' fileField '...'])
@@ -318,3 +335,29 @@ window = getWindow(lenOut, round(0.05*Fs), nCh);
 clipShift = (1-window).*in + window.*clipShift;
 
 kw(padIn+1:padIn+lenOut,:) = clipShift;
+
+function tau = getReverbTime(fileName)
+
+[wav, fs] = audioread(fileName);
+wav = resample(wav, 16, round(fs/1000));
+
+[rt_est,rt_est_mean] = ML_RT_estimation(wav(:,1)', 16e3);
+    
+tau = rt_est_mean;
+
+function xOut = applyRandomIR(x, irDir, irFiles)
+
+orMix = 1.0;
+irMix = 1.0;
+
+% randomly don't apply any IR, but do some random gain
+if randperm(length(irFiles)+1,1)==1
+    xOut = x * (1+rand(1)*0.5);
+    return
+end
+
+irIdx = randperm(length(irFiles), 1);
+
+IR = audioGet(fullfile(irDir, irFiles{irIdx}));
+
+xOut = orMix*x + irMix*applyIR(x, IR);
