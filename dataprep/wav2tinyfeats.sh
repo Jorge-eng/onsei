cc33fcb7fc2f20595864da07dd7b152f0370012a
David J. Klein <david@ersatzlabs.com>
2016-07-14 00:06:07 -0700

    consolitate tinyfeats and python feats; create and save labels

--- dataprep/wav2tinyfeats.sh
+++ dataprep/wav2tinyfeats.sh
@@ -5,18 +5,5 @@
 echo "batchfeat.sh"
 ../implementation/batchfeat.sh $2 $3
 
-if [ $1 = "train" ]; then
-    echo "loadEmbeddedFeatures.py"
-    python loadEmbeddedFeatures.py $3
-
-    echo "zip"
-    rm -f $4\.zip 
-    zip -r $4\.zip spec_pos.mat spec_neg.mat
-
-    echo "copyToHelloServer.sh"
-    ~/copyToHelloServer.sh $4\.zip onsei/net
-elif [ $1 = "test" ]; then
-    echo "createTestGrid.py"
-    python createTestGrid.py $3 $4 
-fi
+./wav2feats.sh $1 $3 $4
 
