d75148cdae1f6a4374a5debaa9f816647811855e
David J. Klein <david@ersatzlabs.com>
2016-07-21 00:02:50 -0700

    consolidate and simplify feature creation API

--- dataprep/wav2tinyfeats.sh
+++
@@ -1,9 +0,0 @@
-# Example: 
-# $ ./wav2tinyfeats.sh train ~/Dropbox/Data/keyword/trainingWavs ~/Dropbox/Data/keyword/trainingFeats trainingFeats kw1 kw2 ...
-# $ ./wav2tinyfeats.sh test ~/Dropbox/Data/keyword/testingWavs ~/Dropbox/Data/keyword/testingFeats testingFeats kw1 kw2 ...
-
-echo "batchfeat.sh"
-../implementation/batchfeat.sh $2 $3
-
-./wav2feats.sh $1 $3 $4 ${@:5}
-
