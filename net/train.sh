72a9688e7cf045b0f7802767dafd0ed78ee1a001
David J. Klein <kleinsound@gmail.com>
2016-07-21 18:03:13 +0000

    add normalization

--- net/train.sh
+++ net/train.sh
@@ -1,11 +1,11 @@
 # Usage:
-# ./train.sh inTag modelDef modelType
+# ./train.sh inTag modelDef modelType normalize
 
 inTag=_$1
 inFile=trainingFeats$inTag\.mat
 
-echo "python train_spec.py $inFile $2 $3 $inTag"
-python train_spec.py $inFile $2 $3 $inTag
+echo "python train_spec.py $inFile $2 $3 $inTag $4"
+python train_spec.py $inFile $2 $3 $inTag $4
 
 cd models
 ./zip_model.sh $2$inTag
