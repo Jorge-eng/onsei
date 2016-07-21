076a847efc6a078635cf505e5558bd4be3855963
David J. Klein <kleinsound@gmail.com>
2016-07-21 18:44:09 +0000

    add normalization input; classWeight

--- net/train_spec.py
+++ net/train_spec.py
@@ -1,8 +1,6 @@
-'''Train a simple deep CNN on the audio mfc dataset.
-
+'''
 GPU run command:
     THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python2 train_cnn_spec.py
-
 '''
 
 from __future__ import print_function
@@ -11,14 +9,24 @@ from keras.callbacks import ModelCheckpoint
 import data
 import modeldefs
 from scipy.io import savemat
+import numpy as np
+import ast
 import pdb
 
+'''
+python train_spec.py inFile modelName modelType modelTag
+'''
+
 # .mat file expected to contain features_pos, labels_pos, features_neg, labels_neg
-inFile = sys.argv[1] 
+inFile = sys.argv[1]
 
 modelName = sys.argv[2]
 modelType = sys.argv[3]
 modelTag = sys.argv[4]
+if len(sys.argv) > 5:
+    normalize = ast.literal_eval(sys.argv[5])
+else:
+    normalize = True
 
 batchSize = 8
 numEpoch = 100
@@ -30,8 +38,10 @@ modelInfo = 'models/'+modelName+modelTag+'.mat'
 (feaTrain, labelTrain), (feaTest, labelTest), (offset, scale) = data.load_training(
 	inFile, modelType,
 	negRatioTrain=10, negRatioTest=10,
-	permuteBeforeSplit=(True,True), testSplit=0.15, normalize=None)
+	permuteBeforeSplit=(True,True), testSplit=0.15, normalize=normalize)
 
+print(modelInfo)
+print('normalize:', normalize)
 print('feaTrain shape:', feaTrain.shape)
 print(feaTrain.shape[0], 'train samples')
 print(feaTest.shape[0], 'test samples')
@@ -54,9 +64,17 @@ model = build_model(inputShape, numClasses)
 cbks = [ModelCheckpoint(modelWeights,
         monitor='val_loss', save_best_only=True, mode='auto')]
 
+if True:
+    classCount = labelTrain.sum(axis=0)
+    w = np.concatenate(([1.], classCount[1:].max() / classCount[1:]))
+    classWeight = dict([(i, w[i]) for i in range(numClasses)])
+else:
+    classWeight = {0:1,1:1,2:10}
+
 model.fit(feaTrain, labelTrain, batch_size=batchSize,
           nb_epoch=numEpoch, show_accuracy=True,
-          validation_data=(feaTest, labelTest), callbacks=cbks, shuffle=True)
+          validation_data=(feaTest, labelTest), callbacks=cbks, shuffle=True,
+          class_weight=classWeight)
 
 if modelType == 'cnn':
     winLen = inputShape[2]
