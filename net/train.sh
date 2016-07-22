c363ad8daa6b3e49b442ac2b172b4ea336a23989
David J. Klein <kleinsound@gmail.com>
2016-07-22 18:02:33 +0000

    add outTag input

--- net/train.sh
+++ net/train.sh
@@ -1,13 +1,14 @@
 # Usage:
-# ./train.sh inTag modelDef modelType normalize
+# ./train.sh inTag outTag modelDef modelType normalize w1,w2,...
 
 inTag=_$1
+outTag=_$2
 inFile=trainingFeats$inTag\.mat
 
-echo "python train_spec.py $inFile $2 $3 $inTag $4 $5"
-python train_spec.py $inFile $2 $3 $inTag $4 $5
+echo "python train_spec.py $inFile $3 $4 $inTag$outTag $5 $6"
+python train_spec.py $inFile $3 $4 $inTag$outTag $5 $6
 
 cd models
-./zip_model.sh $2$inTag
+./zip_model.sh $3$inTag$outTag
 cd -
 
