aff21a7589501c30b3b7a5c070907adb035bee32
David J. Klein <david@ersatzlabs.com>
2016-08-16 00:27:11 -0700

    adding functionality for timedist

--- dataprep/compileTestResults.m
+++ dataprep/compileTestResults.m
@@ -1,9 +1,21 @@
 function [decMat, probMat, idMatch, condMatch] = compileTestResults(probFile, feaFile, th, ep)
 
 load(probFile) % -> prob
-if ndims(prob) == 3
-    prob = squeeze(prob(ep,:,:));
+
+if ndims(prob) == 3 
+    if exist('ep','var')
+        prob = squeeze(prob(ep,:,2:end));
+    else
+        nClass = size(prob,3);
+        for j = 2:nClass
+            score(:,:,j-1) = (prob(:,:,j) > 0.2) & (prob(:,:,j) > max(prob(:,:,setdiff(2:nClass,j)),[],3));
+        end
+        prob = squeeze(sum(score,2));
+    end
+else
+    prob = prob(:,2:end);
 end
+
 load(feaFile); % -> idMatch, condMatch, identity, sampleType
 
 idMatch = cellstr(idMatch);
@@ -17,8 +29,8 @@ cond = cellstr(sampleType);
 decMat = zeros(nId, nCond);
 probMat = zeros(nId, nCond);
 
-for l = 1:size(prob, 2)-1
-    p = prob(:,l+1);
+for l = 1:size(prob, 2)
+    p = prob(:,l);
     isMax = (p == max(prob,[],2));
     for j = 1:nId
         for k = 1:nCond
