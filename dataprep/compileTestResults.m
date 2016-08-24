df21c6932a79d9056973317215e60851542bf454
David J. Klein <david@ersatzlabs.com>
2016-08-24 13:32:41 -0700

    don't reload prob data for each epoch

--- dataprep/compileTestResults.m
+++ dataprep/compileTestResults.m
@@ -1,19 +1,23 @@
-function [decMat, probMat, idMatch, condMatch] = compileTestResults(probFile, feaFile, th, ep)
+function [decMat, data] = compileTestResults(probFile, feaFile, th, ep, data)
 
-load(probFile) % -> prob
-
-if ndims(prob) == 3 
-    if exist('ep','var')
-        prob = squeeze(prob(ep,:,2:end));
-    else
-        nClass = size(prob,3);
-        for j = 2:nClass
-            score(:,:,j-1) = (prob(:,:,j) > 0.2) & (prob(:,:,j) > max(prob(:,:,setdiff(2:nClass,j)),[],3));
-        end
-        prob = squeeze(sum(score,2));
-    end
+if ~exist('data','var') || isempty(data)
+    load(probFile) % -> prob
+    data = prob;
 else
-    prob = prob(:,2:end);
+    prob = data;
+end
+%prob = prob(:,:,:,1:2);
+
+nd = ndims(prob);
+if nd == 4 || (nd == 3 && ~exist('ep','var'))
+    if nd == 4
+        prob = squeeze(max(prob(:,:,end-20:end,:),[],3));
+    elseif nd == 3
+        prob = squeeze(max(prob(:,end-20:end,:),[],2));
+    end
+end
+if exist('ep','var')
+    prob = reshape(prob(ep,:,:),[size(prob,2) size(prob,3)]);
 end
 
 load(feaFile); % -> idMatch, condMatch, identity, sampleType
@@ -29,15 +33,15 @@ cond = cellstr(sampleType);
 decMat = zeros(nId, nCond);
 probMat = zeros(nId, nCond);
 
-for l = 1:size(prob, 2)
-    p = prob(:,l);
+for l = 1:size(prob, 2)-1
+    p = prob(:,l+1);
     isMax = (p == max(prob,[],2));
     for j = 1:nId
         for k = 1:nCond
             
             ii = strcmp(idMatch{j}, id) & strcmp(condMatch{k}, cond);
             dec = p(ii) > th;
-            if 1
+            if 0
                 dec = dec & isMax(ii);
             end
             posRate = sum(dec) / length(dec);
