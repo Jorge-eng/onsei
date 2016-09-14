4b8721ed2c1af7f128356b3e27cfe5e9ddc8cf20
David J. Klein <david@ersatzlabs.com>
2016-09-14 14:45:58 -0700

    keeping up with time distribted nets

--- dataprep/analyzeTestResults.m
+++ dataprep/analyzeTestResults.m
@@ -1,36 +1,62 @@
 
-probFile = '../net/prob_all_lstm_small_dist_okay_sense_tiny_819_dist3.mat';
-testFile = 'testingFeats_okay_sense_tiny.mat';
+probFile = '../net/prob_all_lstm_small_dist_okay_sense+stop+snooze_tiny_830.mat';
+testFile = 'testingFeats_okay_sense+stop+snooze_tiny.mat';
 data = [];
-for ep = 51:300
-    [mat, data] = compileTestResults(probFile, testFile, 0.2, ep, data);
+for ep = 1:300
+    [mat, num, tot, data] = compileTestResults(probFile, testFile, 0.2, ep, data);
     for j = 1:size(mat,3), subplot(size(mat,3),1,j), imagesc(mat(:,:,j),[0 1]), end
     ep, pause
 end
 %%
-clear cd1 cd2 fa
-probFile = '../net/prob_all_lstm_small_okay_sense_tiny_819_15.mat';
-testFile = 'testingFeats_okay_sense_tiny.mat';
+clear cd1 cd2 fa cm
+probFile = '../net/prob_all_lstm_small_dist_okay_sense+stop+snooze_tiny_830.mat';
+testFile = 'testingFeats_okay_sense+stop+snooze_tiny.mat';
 decTh = 0.5;
 data = [];
+figure
 for ep = 1:300, ep
-    [mat,data] = compileTestResults(probFile,testFile, decTh, ep, data);
+    [mat,num,tot,data] = compileTestResults(probFile,testFile, decTh, ep, data);
+    for j = 1:size(mat,3), subplot(size(mat,3),1,j), imagesc(mat(:,:,j),[0 1]), end, drawnow
     mat(isnan(mat)) = 0;
     cd1(ep) = 0;
     cd2(ep) = 0;
     fa(ep) = 0;
     for j = 1:size(mat,3)
+        %if j == 2, continue; end        
         cd1(ep) = cd1(ep) + sum(mat(:,j,j)>0.7);
         cd2(ep) = cd2(ep) + sum(mat(:,j,j));
-        fa(ep) = sum(sum(mat(:,setdiff(1:end, j),j)));
+        fa(ep) = fa(ep) + sum(sum(mat(:,setdiff(1:end, j),j)));
     end
 end
 %%
-clear mats
+clear mats cm nn
+data = [];
 for ep = 1:length(ii)
     %figure
-    [mat,data] = compileTestResults(probFile, testFile, decTh, ii(ep), data);
+    [mat,num,tot,data] = compileTestResults(probFile, testFile, decTh, ii(ep), data);
     mats(:,:,:,ep) = mat;
+    figure, set(gcf,'WindowStyle','docked')
     for j = 1:size(mat,3), subplot(size(mat,3),1,j), imagesc(mat(:,:,j),[0 1]), end
-    ii(ep), cd2(ii(ep)), fa(ii(ep)), pause
+    ii(ep),% cd2(ii(ep)), fa(ii(ep)), pause
+    
+        %cm = zeros(size(mat,3),size(mat,3)+1,ep);
+    for j = 1:size(mat,3)
+        for k = 1:size(mat,3)
+            cm(j,k,ep) = sum(num(:,k,j)) / sum(tot(:,k,j));
+            nn(j,k,ep) = sum(tot(:,k,j));
+        end
+        cm(j,size(mat,3)+1,ep) = sum(sum(num(:,k+1:end,j))) / sum(sum(tot(:,k+1:end,j)));
+        nn(j,size(mat,3)+1,ep) = sum(sum(tot(:,k+1:end,j)));
+    end
 end
+% using max: 157, 277, 298
+% using mean: 150, 259, 277, 289, 298, 300
+
+%Large:
+% 104, 105
+% 70, 77, 96, 105, 266
+
+% TOP 3: 105, 96, 266
+
+%ed:
+% 175, 217
--- dataprep/compileTestResults.m
+++ dataprep/compileTestResults.m
@@ -1,4 +1,4 @@
-function [decMat, data] = compileTestResults(probFile, feaFile, th, ep, data)
+function [decMat, numMat, totMat, data] = compileTestResults(probFile, feaFile, th, ep, data)
 
 if ~exist('data','var') || isempty(data)
     load(probFile) % -> prob
@@ -6,14 +6,16 @@ if ~exist('data','var') || isempty(data)
 else
     prob = data;
 end
-%prob = prob(:,:,:,1:2);
+prob = prob(:,:,:,1:4);
 
 nd = ndims(prob);
 if nd == 4 || (nd == 3 && ~exist('ep','var'))
     if nd == 4
-        prob = squeeze(max(prob(:,:,end-20:end,:),[],3));
+        %prob = squeeze(max(prob(:,:,end-7:end,:),[],3));
+        prob = squeeze(mean(prob(:,:,end-7:end,:),3));
     elseif nd == 3
-        prob = squeeze(max(prob(:,end-20:end,:),[],2));
+        %prob = squeeze(max(prob(:,end-7:end,:),[],2));
+        prob = squeeze(mean(prob(:,:,end-7:end,:),3));
     end
 end
 if exist('ep','var')
@@ -30,8 +32,8 @@ nCond = length(condMatch);
 id = cellstr(identity);
 cond = cellstr(sampleType);
 
-decMat = zeros(nId, nCond);
-probMat = zeros(nId, nCond);
+decMat = zeros(nId, nCond, size(prob,2)-1);
+numMat = zeros(nId, nCond, size(prob,2)-1);
 
 for l = 1:size(prob, 2)-1
     p = prob(:,l+1);
@@ -47,8 +49,8 @@ for l = 1:size(prob, 2)-1
             posRate = sum(dec) / length(dec);
             
             decMat(j, k, l) = posRate;
-            probMat(j, k, l) = mean(p(ii));
-            
+            numMat(j, k, l) = sum(dec);
+            totMat(j, k, l) = length(dec);
         end
     end
 end
