f5300c74dbecd8219647941f279a7102bae3a869
David J. Klein <david@ersatzlabs.com>
2016-09-27 15:38:25 -0700

    bugfixes & remove matcher

--- implementation/wavrunner_all_epochs.sh
+++ implementation/wavrunner_all_epochs.sh
@@ -1,20 +1,20 @@
-# ./wavrunner_all_epochs.sh inDir matcher modelTag outTag
+# ./wavrunner_all_epochs.sh inDir modelTag outTag
 
 inDir=$1
-matcher=$2
-modelTag=$3
-outTag=$4
-tmpDir=~/keyword/$4
+modelTag=$2
+outTag=$3
+tmpDir=~/keyword/$3
 
 mkdir -p tmpDir
 rm $tmpDir/*.csv $tmpDir/*.mat
 
-for fn in `ls ../net/models/$modelTag_ep*.c | sort -u`; do
+for fn in `ls ../net/models/$modelTag\_ep*.c | sort -u`; do
     echo $fn
     modelName=`echo $fn | xargs -I{} basename {} | cut -d. -f1`
     ln -sf $fn model_def.c
-    make clean wavrunner 1> /dev/null
-    ./batchrunner.sh $inDir $matcher $tmpDir
+    touch wavrunner.cpp
+    make wavrunner 1> /dev/null
+    ./batchrunner.sh $inDir $tmpDir
     python ../dataprep/collect_wavrunner_files.py $tmpDir prob_$modelName
 done
 
