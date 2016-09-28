82fbed7c6a19ab1615bd57d7141f3353edc143ac
David J. Klein <david@ersatzlabs.com>
2016-09-27 18:04:41 -0700

    info about conditions & keywords

--- dataprep/collect_wavrunner_epochs.py
+++ dataprep/collect_wavrunner_epochs.py
@@ -1,6 +1,7 @@
 import sys, os, glob
 from scipy.io import loadmat, savemat
 import numpy as np
+from createTrainingFeatures import get_conditions
 import pdb
 
 """
@@ -9,6 +10,10 @@ python collect_wavrunner_epochs.py inDir outFile
 
 inDir = sys.argv[1]
 outFile = sys.argv[2]+'.mat'
+if len(sys.argv) > 3:
+    kws = sys.argv[3].split('+')
+else:
+    kws = None
 
 allFiles = glob.glob(os.path.join(inDir,'*.mat'))
 allFiles.sort()
@@ -18,7 +23,17 @@ for fn in allFiles:
     data = loadmat(fn)['prob']
     prob.append(data)
 
+inFiles = loadmat(allFiles[0])['allFiles']
+pos, neg = get_conditions(kws=kws)
+condMatch = pos + neg
+sampleType = []
+for c in condMatch:
+    matched = [c in x for x in inFiles]
+    if matched.count(True) > 0:
+        sampleType.extend([c]*matched.count(True))
+#condMatch = list(set(sampleType))
+
 #prob = np.rollaxis(np.array(prob), 0, 3)
 prob = np.array(prob)
-savemat(outFile, {'prob':prob})
+savemat(outFile, {'prob':prob,'condMatch':condMatch,'sampleType':sampleType})
 
