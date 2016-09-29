e8d4509bd1b116401fe1525ef096b40938c5cfbe
David J. Klein <david@ersatzlabs.com>
2016-09-29 11:06:32 -0700

    input model

--- implementation/batchrunner.sh
+++ implementation/batchrunner.sh
@@ -2,8 +2,17 @@
 
 inDir=$1
 outDir=$2
+modelFile=$3
 
 mkdir -p $outDir
+rm $outDir/*.csv
 
-find $inDir -type f -iname \*.wav | sort | xargs -I{} basename {} | xargs -I{} -P4 sh -c "../implementation/wavrunner '$inDir/{}' > '$outDir/{}.csv'"
+cd ../implementation
 
+ln -sf $modelFile model_def.c
+touch wavrunner.cpp
+make wavrunner 1> /dev/null
+
+find $inDir -type f -iname \*.wav | sort | xargs -I{} basename {} | xargs -I{} -P4 sh -c "./wavrunner '$inDir/{}' > '$outDir/{}.csv'"
+
+cd -
