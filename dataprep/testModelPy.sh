94c42f99c3cc9bbf98422d61a2f2742229e2b50c
David J. Klein <david@ersatzlabs.com>
2016-10-11 12:37:23 -0700

    big model test with tinyfeats and python model

---
+++ dataprep/testModelPy.sh
@@ -0,0 +1,32 @@
+
+modelName=$1
+mkdir -p ../net/outputs/$modelName\_ep$2
+
+#testDirs="reverberant_speech/16k"
+testDirs="TED"
+
+#testDirs="testingWavs_kwClip_okay_sense
+#testingWavs_kwClip_stop
+#testingWavs_kwClip_snooze
+#noiseDataset/16k
+#reverberant_speech/16k"
+
+#testingWavs_adversarial
+
+#testDirs="testingWavs_kwClip_okay_sense
+#testingWavs_kwClip_stop
+#testingWavs_kwClip_snooze
+#noiseDataset/16k"
+
+##speechDataset/16k"
+
+for td in $testDirs; do
+    echo $td
+    mkdir -p ../net/outputs/$modelName\_ep$2/$td
+    if [ $3 = "make_input" ]; then
+        mkdir -p ~/keyword/$td/bin
+        ../implementation/batchfeat.sh ~/keyword/$td ~/keyword/$td/bin
+    fi
+    python ../net/predict_all_files.py ~/keyword/$td/bin ../net/outputs/$modelName\_ep$2/$td $modelName $2
+done
+
