0d77098cd6b93f4b851478a41f949dfc45ac767d
David J. Klein <david@ersatzlabs.com>
2016-10-14 13:10:37 -0700

    limit find to top level directory

--- implementation/batchfeat.sh
+++ implementation/batchfeat.sh
@@ -1,4 +1,4 @@
 mkdir -p $2
 cp $1/*.csv $2
 
-find $1 -type f -iname \*.wav | xargs -I{} basename {} | xargs -I{} -P4 ../implementation/tinyfeats $1/{} $2/{}.bin
+find $1 -maxdepth 1 -type f -iname \*.wav | xargs -I{} basename {} | xargs -I{} -P4 ../implementation/tinyfeats $1/{} $2/{}.bin
