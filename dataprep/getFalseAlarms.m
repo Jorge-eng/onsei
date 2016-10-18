199a14b0ee6630d3f32272dccaa24fd0e3e970ab
David J. Klein <david@ersatzlabs.com>
2016-10-17 17:04:23 -0700

    fa_pool creation

--- dataprep/getFalseAlarms.m
+++ dataprep/getFalseAlarms.m
@@ -1,3 +1,4 @@
+if 0
 kw = 'snooze';
 iter = '22';
 inTag = 'iter';
@@ -30,4 +31,62 @@ for j = 1:length(ii), j
     %end
     %lastfile = file;
     
+end
+end
+%%
+modelName = 'model_aug30_lstm_med_dist_okay_sense+stop+snooze_tiny_fa_1006_ep077';
+%kws = {'okay_sense','stop','snooze'};
+kws = {'okay_sense'};
+negDirs = {'fa_pool'};
+file = {};
+det = [];
+fileType = 'mat';
+c = 1; 
+[~,~] = mkdir('~/keyword/fa_pool/clips');
+for k = 1:length(negDirs)
+    dirName = fullfile('../net/outputs',modelName,negDirs{k})
+
+    d = dir(fullfile(dirName, ['*.' fileType]));
+    h = ones(3, 1, 'single');
+    th_pct = 0.5;
+    
+    for j = 1:length(d)
+        disp(d(j).name)
+        if strcmp(fileType, 'csv')
+            data = csvread(fullfile(dirName, d(j).name));
+            th = th_pct * 4095;
+        elseif strcmp(fileType, 'mat')
+            data = load(fullfile(dirName, d(j).name));
+            data = squeeze(data.prob);
+            th = th_pct;
+        end
+        
+        [num, loc] = runnerDetector(data(:,2:end), h, th);
+        for kw = 1:length(kws)
+            for l = 1:num(kw)
+                file{end+1} = fullfile(dirName, d(j).name);
+                det(end+1) = loc{kw}(l);
+                
+                if 1
+                    wavFile = strrep(file{end},fullfile('../net/outputs',modelName),'~/keyword');
+                    if strcmp(fileType, 'csv')
+                        wavFile = strrep(wavFile,'.csv','');
+                    elseif strcmp(fileType, 'mat')
+                        wavFile = strrep(wavFile,'.bin.mat','');
+                    end
+                    
+                    lim = [1 1.6*16e3] + round(16e3*(det(end)*0.015-1.5));
+                    if lim(1) < 1
+                        lim = lim - lim(1) + 1;
+                    end
+                    wav = audioread(wavFile,lim);
+                    outFile = ['~/keyword/fa_pool/clips/falseAlarmClip_' kws{kw} '_' num2str(c) '.wav'];
+                    disp(outFile)
+                    audiowrite(outFile,wav,16e3);
+                    c = c + 1;
+                end
+            end
+        end
+        
+    end
 end
