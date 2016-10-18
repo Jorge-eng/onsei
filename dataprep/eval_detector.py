7c26b989d2aca1f7a3d440e93725db807a73d766
David J. Klein <david@ersatzlabs.com>
2016-10-18 16:29:31 -0700

    eval.py -> eval_detector.py

--- dataprep/eval_detector.py
+++ dataprep/eval_detector.py
@@ -20,21 +20,8 @@ def detector(data, h, th):
 
     return num
 
-#data = np.genfromtxt(sys.argv[1], delimiter=',')
-netOutDir = os.path.join('../net/outputs', sys.argv[1])
-testDirs = ['testingWavs_kwClip_okay_sense',
-            'testingWavs_kwClip_stop',
-            'testingWavs_kwClip_snooze',
-            'noiseDataset/16k',
-            'reverberant_speech/16k',
-            'TED']
+def eval_dir(inDir, ths, counts, nKw):
 
-ths = np.array([0.2, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98])
-counts = np.arange(1, 16, 2)
-nKw = 3
-
-for td in testDirs:
-    inDir = os.path.join(netOutDir, td)
     allFiles = glob.glob(os.path.join(inDir,'*.mat'))
     allFiles.sort()
 
@@ -50,8 +37,36 @@ for td in testDirs:
             for n, th in enumerate(ths):
                 num[j,m,n,:] = detector(data[:,1:1+nKw], h, th)
 
-    #np.savetxt(sys.argv[4], fmt='%0.5f', delimiter=',')
-    outFile = os.path.join(netOutDir, 'eval_'+td.split('/')[0]+'.mat')
+    return num, tot
 
-    savemat(outFile, {'tot':tot,'num':num})
+def eval_all(modelTag, nKw=3):
+
+    testDirs, ths, counts = params()
+    netOutDir = os.path.join('../net/outputs', modelTag)
+
+    for td in testDirs:
+
+        inDir = os.path.join(netOutDir, td)
+        num, tot = eval_dir(inDir, ths, counts, nKw)
+
+        outFile = os.path.join(netOutDir, 'eval_'+td.split('/')[0]+'.mat')
+        savemat(outFile, {'tot':tot,'num':num})
+
+def params():
+
+    testDirs = ['testingWavs_kwClip_okay_sense',
+                'testingWavs_kwClip_stop',
+                'testingWavs_kwClip_snooze',
+                'noiseDataset/16k',
+                'reverberant_speech/16k',
+                'TED']
+
+    ths = np.array([0.2, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.98])
+    counts = np.arange(1, 16, 2)
+
+    return testDirs, ths, counts
+
+if __name__ == '__main__':
+
+    eval_all(sys.argv[1])
 
