a0906fe23166a824b8454ae747cc239c6acaa24e
David J. Klein <david@ersatzlabs.com>
2016-10-18 18:03:44 -0700

    get predictions for all files in a directory

---
+++ net/predict_all_files.py
@@ -0,0 +1,48 @@
+import sys, os, glob
+from scipy.io import loadmat, savemat
+from predict_spec import get_model, get_input
+import numpy as np
+import pdb
+
+try: # pyplot throws errors on ec2
+    import matplotlib.pyplot as plt
+except:
+    print('Warning: pyplot failed to import')
+    pass
+
+# Usage:
+# $ python predict_all_files.py path/to/inputs path/to/outputs model_name [epoch]
+
+inDir = sys.argv[1]
+outDir = sys.argv[2]
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
+
+files = glob.glob(os.path.join(inDir,'*.wav'))
+if len(files) > 0:
+    inType = 'audio'
+else:
+    files = glob.glob(os.path.join(inDir,'*.bin'))
+    inType = 'tinyfeats'
+
+if len(files)==0:
+    raise ValueError('No valid files found in '+inDir)
+
+modelTag = sys.argv[3]
+if len(sys.argv) > 4:
+    epoch = int(sys.argv[4])
+else:
+    epoch = None
+
+model, modelType, winLen, offset, scale = get_model(modelTag, epoch=epoch)
+
+for fn in files:
+
+    feaStream = get_input(fn, inType, modelType, offset=offset, scale=scale, winLen=winLen)
+
+    prob = model.predict_proba(feaStream, verbose=1)
+
+    outFile = os.path.join(outDir, os.path.basename(fn)+'.mat')
+    print('Saving '+outFile)
+    savemat(outFile, {'prob': prob})
+
