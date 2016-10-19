f7ba039215737ca6b419600f400b573157e6f0e7
David J. Klein <david@ersatzlabs.com>
2016-10-18 23:05:12 -0700

    debug; add nKw input

--- dataprep/full_eval.py
+++ dataprep/full_eval.py
@@ -11,6 +11,8 @@ import numpy as np
 import pdb
 
 modelTag = sys.argv[1]
+nKw = eval(sys.argv[2]) if len(sys.argv) > 2 else 3
+
 model, modelType, winLen, offset, scale = get_arch(modelTag)
 
 tag = os.path.join(MODEL_PATH, modelTag)
@@ -22,14 +24,15 @@ testDirs, ths, counts = eval_detector.params()
 for wf in weightFiles:
     modelTag = wf.replace('.h5','')
 
-    netOutDir = os.path.join(NET_PATH, 'outputs', modelTag)
+    netOutDir = os.path.join(NET_PATH, 'outputs', os.path.basename(modelTag))
     if not os.path.exists(netOutDir):
         os.makedirs(netOutDir)
 
+    print('Loading '+wf)
     model.load_weights(wf)
 
     for td in testDirs:
-        files = glob.glob(os.path.join('~/keyword', td, 'bin', '*.bin'))
+        files = glob.glob(os.path.expanduser(os.path.join('~/keyword', td, 'bin', '*.bin')))
         if len(files)==0:
             raise ValueError('No valid files found in '+inDir)
 
@@ -37,9 +40,12 @@ for wf in weightFiles:
             feaStream = get_input(fn, 'tinyfeats', modelType, offset=offset, scale=scale, winLen=winLen)
             prob = model.predict_proba(feaStream, verbose=1)
 
-            outFile = os.path.join(netOutDir, td, os.path.basename(fn)+'.mat')
+            testDir = os.path.join(netOutDir, td)
+            if not os.path.exists(testDir):
+                os.makedirs(testDir)
+            outFile = os.path.join(testDir, os.path.basename(fn)+'.mat')
             print('Saving '+outFile)
             savemat(outFile, {'prob': prob})
 
-    eval_detector.eval_all(netOutDir, nKw=3, deleteInput=True)
+    eval_detector.eval_all(netOutDir, nKw=nKw, deleteInput=True)
 
