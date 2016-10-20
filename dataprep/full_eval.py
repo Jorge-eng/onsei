afea8e3b244a18920e9b78c5b16631056d1abe32
David J. Klein <david@ersatzlabs.com>
2016-10-19 22:04:00 -0700

    more parallelization work

--- dataprep/full_eval.py
+++ dataprep/full_eval.py
@@ -4,7 +4,8 @@ NET_PATH = os.path.join(TOP_DIR, '../net')
 MODEL_PATH = os.path.join(NET_PATH, 'models')
 sys.path.append(NET_PATH)
 
-from scipy.io import savemat
+from keras.models import model_from_json
+from scipy.io import loadmat, savemat
 from predict_spec import get_arch, get_input
 import eval_detector
 import numpy as np
@@ -12,22 +13,45 @@ import multiprocessing
 from joblib import Parallel, delayed
 import pdb
 
-def file_prob(model, fn, modelType, offset, scale, winLen, testDir):
-    feaStream = get_input(fn, 'tinyfeats', modelType, offset=offset, scale=scale, winLen=winLen)
-    prob = model.predict_proba(feaStream, verbose=1)
+def file_prob(model, files, modelType, offset, scale, winLen, testDir):
+    for fn in files:
+        feaStream = get_input(fn, 'tinyfeats', modelType, offset=offset, scale=scale, winLen=winLen)
+        prob = model.predict_proba(feaStream, verbose=1)
 
-    outFile = os.path.join(testDir, os.path.basename(fn)+'.mat')
-    print('Saving '+outFile)
-    savemat(outFile, {'prob': prob})
+        outFile = os.path.join(testDir, os.path.basename(fn)+'.mat')
+        print('Saving '+outFile)
+        savemat(outFile, {'prob': prob})
 
+def chunks(l, n):
+    z = int(np.ceil(np.float(len(l)) / n))
+    for i in xrange(0, len(l), z):
+        yield l[i:i + z]
 
-num_cores = 1#multiprocessing.cpu_count()
+def get_model_info(modelTag):
+
+    infoFile = os.path.join(MODEL_PATH, modelTag+'.mat')
+    info = loadmat(infoFile)
+
+    modelDef = os.path.join(NET_PATH, info['modelDef'][0])
+    modelType = info['modelType'][0]
+    winLen = int(info['winLen'][0])
+    offset = info['offset'][0]
+    scale = info['scale'][0]
+
+    return modelDef, modelType, winLen, offset, scale
+
+def compile_model(modelDef):
+    return model_from_json(open(modelDef).read())
+
+nWorkers = np.minimum(8, multiprocessing.cpu_count())
 sys.setrecursionlimit(10000)
 
 modelTag = sys.argv[1]
 nKw = eval(sys.argv[2]) if len(sys.argv) > 2 else 3
 
-model, modelType, winLen, offset, scale = get_arch(modelTag)
+modelDef, modelType, winLen, offset, scale = get_model_info(modelTag)
+
+models = Parallel(n_jobs=nWorkers)(delayed(compile_model)(modelDef) for _ in range(nWorkers))
 
 tag = os.path.join(MODEL_PATH, modelTag)
 weightFiles = glob.glob(tag+'_ep*.h5')
@@ -43,7 +67,8 @@ for wf in weightFiles:
         os.makedirs(netOutDir)
 
     print('Loading '+wf)
-    model.load_weights(wf)
+    for model in models:
+        model.load_weights(wf)
 
     for td in testDirs:
         files = glob.glob(os.path.expanduser(os.path.join('~/keyword', td, 'bin', '*.bin')))
@@ -54,9 +79,7 @@ for wf in weightFiles:
         if not os.path.exists(testDir):
             os.makedirs(testDir)
 
-        Parallel(n_jobs=num_cores)(delayed(file_prob)(model, fn, modelType, offset, scale, winLen, testDir) for fn in files)
-        #for fn in files:
-        #    file_prob(model, fn, modelType, offset, scale, winLen, testDir)
+        Parallel(n_jobs=nWorkers)(delayed(file_prob)(model, fn, modelType, offset, scale, winLen, testDir) for model, fn in zip(models, chunks(files, nWorkers)))
 
     eval_detector.eval_all(netOutDir, nKw=nKw, deleteInput=True)
 
