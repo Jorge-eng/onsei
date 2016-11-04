ea5eb1f262a074ebb6571d910b2e0be9d856808a
David J. Klein <david@ersatzlabs.com>
2016-11-03 17:28:17 -0700

    refactor for more automation

--- dataprep/batchAnnotate.m
+++ dataprep/batchAnnotate.m
@@ -1,28 +1,52 @@
-function batchAnnotate(dirName, ext, kw, clipDataFile, outWavDir)
-% batchAnnotate(dirName, ext, kw, clipDataFile, outWavDir)
+function batchAnnotate(kw, dirName, outWavDir, clipDataFile, ext)
+% batchAnnotate(kw, dirName, outWavDir, clipDataFile, ext)
 %
+% kw (string) - name of the keyword
+% dirName (optional) - location of the input audio files
+% outWavDIr (optional) - path the the output audio clip folder ([] if none)
+% clipDataFile (optional) - path to the output .mat file containing info
+%                           about the audio clips ([] if none)
+% ext (optional) - file extention of the audio files
+% 
 % Example:
-% batchAnnotate('~/Dropbox/Data/keyword/recordings/toAnnotateSense','wav','okay_sense','~/Dropbox/Data/keyword/clipData_irs.mat','~/Dropbox/Data/keyword/trainingWavsIrs')
+% batchAnnotate('okay_sense')
+
+if ~exist('ext','var')
+    ext = 'wav';
+end
+if ~exist('outWavDir','var')
+    outWavDir = ['~/keyword/trainingWavs_' kw];
+end
+if ~exist('clipDataFile','var')
+    clipDataFile = ['~/keyword/clipData_' kw '.mat'];
+end
+if ~exist('dirName','var')
+    dirName = ['~/keyword/recordings/toAnnotate_' kw];
+end
 
 d = dir(fullfile(dirName,['*.' ext]));
 files = {d.name};
 
+[~,~] = mkdir(outWavDir);
+annotationsDir = fullfile('~/keyword/annotations',kw);
+[~,~] = mkdir(annotationsDir);
+recordingsDir = '~/keyword/recordings';
+
 for j = 1:length(files)
    
     wavName = fullfile(dirName, files{j});
-    fileId = strrep(files{j},'.wav','');
-    [~,~] = mkdir(fullfile('~/Dropbox/Data/keyword/annotations',kw));
-    annoName = fullfile('~/Dropbox/Data/keyword/annotations',kw,['annotations_' fileId '.csv']);
+    fileId = strrep(files{j},['.' ext],'');
+    annoName = fullfile(annotationsDir,['annotations_' fileId '.csv']);
     
     if ~exist(annoName, 'file')
         annotateWav(wavName, annoName);
         annotateKw(dirName, annoName);
     
-        copyfile(fullfile(dirName, files{j}), fullfile('~/Dropbox/Data/keyword/recordings',files{j}))
+        copyfile(fullfile(dirName, files{j}), fullfile(recordingsDir,files{j}))
 
-        if exist('clipDataFile','var')
-            getAnnotatedData('~/Dropbox/Data/keyword/recordings', annoName, clipDataFile);
-            if exist('outWavDir','var')
+        if ~isempty(clipDataFile)
+            getAnnotatedData(recordingsDir, annoName, clipDataFile);
+            if ~isempty(outWavDir)
                 createTrainingWavs(clipDataFile, outWavDir, kw, fileId);
             end
         end
