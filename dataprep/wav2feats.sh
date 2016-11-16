e2427b634d789e2aa79e5a46a74f955f22b4d798
David J. Klein <david@ersatzlabs.com>
2016-11-15 16:59:04 -0800

    adjustments for new conventions

--- dataprep/wav2feats.sh
+++ dataprep/wav2feats.sh
@@ -1,12 +1,12 @@
 # Example: 
-# $ ./wav2feats.sh [train | test] [py | tiny] kw1+kw2+...
+# $ ./wav2feats.sh [train | test] [py | tiny] kw1+kw2+.+kwN tag
 
-dataRoot=~/Dropbox/Data/keyword
-inDir=$dataRoot/$1\ingWavs
-outName=$1\ingFeats_$3\_$2
+dataRoot=~/keyword
+inDir=$dataRoot/$3/$1/wav
+outName=$1\ingFeats_$3\_$2\_$4
 
 if [ $2 = "tiny" ]; then
-    outFeaDir=$dataRoot/$1\ingFeats
+    outFeaDir=$dataRoot/$3/$1/bin
     if [ ! -d "$outFeaDir" ]; then
         echo "batchfeat.sh $inDir $outFeaDir"
         ../implementation/batchfeat.sh $inDir $outFeaDir
@@ -18,7 +18,7 @@ if [ $1 = "train" ]; then
     echo "createTrainingFeatures.py $inDir $outName $3"
     python createTrainingFeatures.py $inDir $outName $3
 
-    echo "copyToHelloServer.sh $outName.mat onsei/net"
+    echo "copyToHelloServer.sh $outName\.mat onsei/net"
     ~/copyToHelloServer.sh $outName\.mat onsei/net
 elif [ $1 = "test" ]; then
     echo "createTestGrid.py $inDir $outName $3" 
