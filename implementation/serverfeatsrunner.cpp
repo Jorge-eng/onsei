ffe9b627bc209514ffa8089e2bee4ed02e64bf9f
Benjamin Joseph <benjo@sayhello.com>
2017-04-03 14:58:36 -0700

    server feats runner now outputs files without slashes

--- implementation/serverfeatsrunner.cpp
+++ implementation/serverfeatsrunner.cpp
@@ -20,18 +20,19 @@
 
 #include "model_def.c"
 
-#define FEATS_SAVE_THRESOLD_OKAY_SENSE (TOFIX(0.50f))
-#define NUM_TIMES_TO_HAVE_EXCEEDED_THRESHOLD (3)
+#define FEATS_SAVE_THRESOLD_OKAY_SENSE (TOFIX(-0.1f))
+#define NUM_TIMES_TO_HAVE_EXCEEDED_THRESHOLD (1)
 
 std::string base64_decode(std::string const& encoded_string);
 std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len);
+bool Encode32(unsigned char* in, int inLen, unsigned char* out);
 
 using namespace std;
 using namespace rapidjson;
 
 
 
-static void do_something_with_the_text(const std::string & str,const std::string desired_keyword) {
+static void do_something_with_the_text(const std::string & str,const std::string desired_keyword,const size_t counter) {
     
     //string to json
     Document d;
@@ -43,12 +44,14 @@ static void do_something_with_the_text(const std::string & str,const std::string
     
     const size_t pos = id.find("+");
     if (pos == std::string::npos) {
+        std::cerr << "failed to find + in id in iter" << counter << std::endl;
         return;
     }
 
     const std::string keyword = id.substr(pos + 1,id.size() - pos + 1);
     
     if (keyword != desired_keyword) {
+        std::cerr << "undesired keyword" << keyword << " at iter " << counter << std::endl;
         return;
     }
     
@@ -80,7 +83,7 @@ static void do_something_with_the_text(const std::string & str,const std::string
     
     const int N = feats.size() / NUM_MEL_BINS;
     bool is_saved = false;
-    
+    int threshold_count = 0;
     for (int i = 0; i < N; i++) {
         
         const int16_t * featvec = &feats[i*NUM_MEL_BINS];
@@ -103,7 +106,6 @@ static void do_something_with_the_text(const std::string & str,const std::string
         }
         std::cout << std::endl;
         
-        static int threshold_count = 0;
         if (tensor_out->x[1] > FEATS_SAVE_THRESOLD_OKAY_SENSE) {
             threshold_count++;
             
@@ -115,6 +117,14 @@ static void do_something_with_the_text(const std::string & str,const std::string
             threshold_count = 0;
         }
         
+    }
+    
+    if (!is_saved) {
+        static int count = 1;
+        std::cerr << "FAILED AT ITERATION " << count << std::endl;
+        count++;
+        int foo = 3;
+        foo++;
     }
     
     if (is_saved) {
@@ -130,13 +140,37 @@ static void do_something_with_the_text(const std::string & str,const std::string
         SHA1Reset(&c);
         SHA1Input(&c,reinterpret_cast<uint8_t *>(feats.data()),data_size_bytes);
         SHA1Result(&c, message_digest);
-     
-        const std::string id = base64_encode(message_digest,sizeof(message_digest)) + ".bin";
+
+        //hex conversion of binary data
+        char idbuf[1 << 10] = {0};
+
+        for (int ibyte = 0; ibyte < sizeof(message_digest); ibyte++) {
+            char temp[10];
+            snprintf(temp,sizeof(temp),"%02x",message_digest[ibyte]);
+            idbuf[2*ibyte] = temp[0];
+            idbuf[2*ibyte + 1] = temp[1];
+
+        }
+
+        int istart = 2*sizeof(message_digest);
+        idbuf[istart] = '.';
+        idbuf[istart + 1] = 'b';
+        idbuf[istart + 2] = 'i';
+        idbuf[istart + 3] = 'n';
+
+        std::string id;// = base64_encode(message_digest,sizeof(message_digest)) + ".bin";
+        id.assign(idbuf,2*sizeof(message_digest) + 5);
+        
         
         std::ofstream fout(id.c_str());
         
+        if (!fout.is_open()) {
+            std::cerr << "FILE OUT " << id << " failed to open file" << std::endl;
+        }
+        
         fout.write(reinterpret_cast<char *>(feats.data()), data_size_bytes);
         fout.close();
+        
     }
     
     count++;
@@ -182,11 +216,18 @@ int main(int argc, char * argv[]) {
     
     size_t prevpos = 0;
     */
-    
+    size_t counter = 0;
     std::string str;
     while (std::getline(file, str)) {
-        do_something_with_the_text(str,argv[2]);
+        do_something_with_the_text(str,argv[2],counter);
+        counter++;
+        
+        if (counter % 100 == 0) {
+            std::cerr << counter << std::endl;
+        }
     }
+    
+    std::cerr << "FINISHED " << counter << " blobs" << std::endl;
     
     /*
     while(1) {
